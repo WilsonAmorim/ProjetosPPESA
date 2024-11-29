55dd926fe9d015ff162e338963446921cf900a00
Wilson Lopes Amorim <wilson.amorim@saebti.intranet>
2024-11-29 15:57:01 -0300

    Primeiro Commit

---
+++ Sqls/InsertLote.sql
@@ -0,0 +1,3 @@
+INSERT INTO sys.tbconvenio (tbConvenioLote) VALUES("Lote 1");
+INSERT INTO sys.tbconvenio (tbConvenioLote) VALUES("Lote 2");
+INSERT INTO sys.tbconvenio (tbConvenioLote) VALUES("Lote 3");
---
+++ Sqls/TBOrgaos.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `sys`.`tborgaos` (
+  `idtborgaos` INT NOT NULL AUTO_INCREMENT,
+  `Sigla` VARCHAR(45),
+  `tborgaosidtbConvenio` INT NOT NULL,
+  `tbOrgaosDescricao` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`idtborgaos`, `tborgaosidtbConvenio`),
+  UNIQUE INDEX `idtborgaos_UNIQUE` (`idtborgaos` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_general_ci
+COMMENT = 'Tabela de orgaos conveniados';
